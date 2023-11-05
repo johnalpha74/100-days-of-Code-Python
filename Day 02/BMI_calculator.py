7ced57f028603c12b42091d2838bfdfadb533e33
johnalpha74 <johnalpha@gmail.com>
2023-11-05 23:20:12 +0200

    CHORE: Calculate BMI)

---
+++ Day 02/BMI_calculator.py
@@ -0,0 +1,9 @@
+# 1st input: enter height in meters e.g: 1.65
+height = input()
+# 2nd input: enter weight in kilograms e.g: 72
+weight = input()
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+bmi = int(weight) / float(height) ** 2
+print(int(BMI))
