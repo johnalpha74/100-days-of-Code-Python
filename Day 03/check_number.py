e0adf365edb75f80751de6a060d72bb9b1738def
johnalpha74 <johnalpha@gmail.com>
2023-11-06 07:23:35 +0200

    CHORE: Is Even or Odd

---
+++ Day 03/check_number.py
@@ -0,0 +1,10 @@
+# Which number do you want to check?
+number = input()
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+print("Please enter a number to check if it's odd or even.")
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
